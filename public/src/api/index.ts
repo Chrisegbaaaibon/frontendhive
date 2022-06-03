da6316770c38b3cf8478eaba07c88a3a11bbeff4
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2022-06-03 22:11:32 +0100

    Updated

--- public/src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const APP_URI = "https://hivend.herokuapp.com";
-
-export default axios.create({ baseURL: `${APP_URI}/api/` });
--- public/src/components/CollabVideoFrame.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from "react";
-
-export const CollabVid: FC = () => {
-  return <video src="./collabo.mp4" autoPlay={true} controls={true} />;
-};
